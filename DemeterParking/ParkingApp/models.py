bc231e37a9c86d5ee40c787bcd422dbec767e796
michaelrejo1601 <ninjapenguin1601@gmail.com>
2020-05-13 22:39:46 -0700

    Added Models and Auth Views

--- DemeterParking/ParkingApp/models.py
+++ DemeterParking/ParkingApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    """Profile Details of User (1:1 User)"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.DecimalField(decimal_places=2)
+class Lot(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    checkin = models.DateTimeField()
+    checkout = models.DateTimeField()
+    rate = models.DecimalField(decimal_places=2)
+    address = models.CharField()
+    city = models.CharField()
+    state = models.CharField(max_length=2)
+    notes = models.TextField()
+    postal = models.IntegerField(max_length=5)
+    #False = Available
+    status = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
